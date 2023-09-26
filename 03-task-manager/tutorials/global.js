8479735b003d144fbfe475cace3a4a4c78e66d30
2sammy <samuelmanoah61@gmail.com>
2023-09-26 22:39:44 +0300

    first commit

---
+++ 03-task-manager/tutorials/global.js
@@ -0,0 +1,16 @@
+/*
+globals - no window*/
+
+// __dirname - path to the current directory
+console.log(__dirname);
+//__filename - file name
+console.log(__filename);
+// require-  function to use modules(common js)
+
+//module - info about current module(file)
+
+//process - info about env where the program is being executed
+setInterval(() =>{
+    console.log('hello sam')
+
+}, 1000)
