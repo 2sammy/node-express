7d2ea30e8f72acdbac5cc885f74a5d8ada48ed7e
2sammy <samuelmanoah61@gmail.com>
2023-09-26 23:59:27 +0300

    second file

---
+++ 03-task-manager/tutorials/3-modules.js
@@ -0,0 +1,9 @@
+//modules
+const names = require('./4-names');
+const sayHi = require('./5-utilis')
+const data = require('./6-alternative')
+console.log(data)
+
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
