e6e7c987167623135ef97f55d1df7ebbb0321834
2sammy <samuelmanoah61@gmail.com>
2023-09-28 08:59:14 +0300

    set interval: loop

--- 03-task-manager/tutorials/app.js
+++ 03-task-manager/tutorials/app.js
@@ -1,8 +1,4 @@
-//operating system process
-console.log('first')
-setTimeout(() => {
-    console.log('second')
-},0)
-console.log('third')
-// completed and exited operating system process
-
+setInterval(() => {
+    console.log('hello world')
+}, 2000)
+console.log('I will run first')
