6cec13bfc3d1624907d40d9986d0d604f510c108
2sammy <samuelmanoah61@gmail.com>
2023-09-28 19:48:05 +0300

    creation of webrouser

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,8 +1,20 @@
 const http = require ('http')
 
 const server = http.createServer((req, res)=> {
-    console.log('user hit the server..')
-    res.end('home page')
+    res.writeHead(200, {'content-type':'text/html'})
+    res.write('<h2> welcome to the home page</h2>')
+    res.end()
+
 })
 
 server.listen(5000)
+
+/*
+const http = require('http)
+const server = http.createServer((req,res) => {
+    console.log('user hit diff...)
+    res.end ('home...)
+})
+
+
+*/ 
