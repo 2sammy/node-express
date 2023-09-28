b58ded7606610a13e808c718de860548988980a2
2sammy <samuelmanoah61@gmail.com>
2023-09-28 23:14:13 +0300

    http setting up server

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,16 +2,18 @@ const http = require ('http')
 const { readFileSync} = require('fs');
 
 //get all files
- const homepage = readFileSync('./index.html')
+ const homepage = readFileSync('./navbar-app/index.html')
+ const homeStyles = readFileSync('./navbar-app/styles.css')
+ const homeImage = readFileSync('./navbar-app/logo.svg')
+ const homeLogic = readFileSync('./navbar-app/browser-app.js')
 
 
 
 const server = http.createServer((req, res)=> {
-   // console.log(req.method)
-   //console.log(req.url)
   
    //home page
    const url = req.url
+   console.log(url)
      if(url === '/' ){
         res.writeHead(200, {'content-type':'text/html'})
     res.write(homepage)
@@ -21,7 +23,35 @@ const server = http.createServer((req, res)=> {
     //about page
     else if(url === '/about' ) {
         res.writeHead(200, {'content-type':'text/html'})
-        res.write('<h2> about the page</h2>')
+        res.write()
+        res.end()
+
+
+    }
+    //styles
+    else if(url === '/styles.css' ) {
+        res.writeHead(200, {'content-type':'text/css'})
+        res.write(homeStyles)
+        res.end()
+
+
+    }
+    else if(url === '/logo.svg' ) {
+        res.writeHead(200, {'content-type':'image/svg+xml'})
+        res.write(homeImage)
+        res.end()
+    }
+    else if(url === '/logo.svg' ) {
+        res.writeHead(200, {'content-type':'image/svg+xml'})
+        res.write(homeImage)
+        res.end()
+
+
+    }
+
+    else if(url === '/browser-app.js' ) {
+        res.writeHead(200, {'content-type':'text/javascript'})
+        res.write(homeLogic)
         res.end()
 
 
