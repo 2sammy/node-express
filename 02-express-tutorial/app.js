137ffcbb28b609c7a2ba9cf5514b5bf3941e35d7
2sammy <samuelmanoah61@gmail.com>
2023-09-28 20:05:32 +0300

    creation of webrouser

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,9 +1,31 @@
 const http = require ('http')
+const { url } = require('inspector')
 
 const server = http.createServer((req, res)=> {
-    res.writeHead(200, {'content-type':'text/html'})
+   // console.log(req.method)
+   //console.log(req.url)
+  
+   //home page
+     if(url ==='/' ){
+        res.writeHead(200, {'content-type':'text/html'})
     res.write('<h2> welcome to the home page</h2>')
     res.end()
+
+    }
+    //about page
+    else if(url === '/about' ) {
+        res.writeHead(200, {'content-type':'text/html'})
+        res.write('<h2> about the page</h2>')
+        res.end()
+
+
+    }
+    //404
+    else{
+        res.writeHead(400, {'content-type':'text/html'})
+        res.write('<h2> no home page</h2>')
+        res.end()
+    }
 
 })
 
