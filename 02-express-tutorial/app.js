b01e23751e468e3880af175b3765035dbd361289
2sammy <samuelmanoah61@gmail.com>
2023-09-29 08:01:34 +0300

    basic express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,78 +1,18 @@
-const http = require ('http')
-const { readFileSync} = require('fs');
+const express = require('express')
 
-//get all files
- const homepage = readFileSync('./navbar-app/index.html')
- const homeStyles = readFileSync('./navbar-app/styles.css')
- const homeImage = readFileSync('./navbar-app/logo.svg')
- const homeLogic = readFileSync('./navbar-app/browser-app.js')
+const app = express()
 
-
-
-const server = http.createServer((req, res)=> {
-  
-   //home page
-   const url = req.url
-   console.log(url)
-     if(url === '/' ){
-        res.writeHead(200, {'content-type':'text/html'})
-    res.write(homepage)
-    res.end()
-
-    }
-    //about page
-    else if(url === '/about' ) {
-        res.writeHead(200, {'content-type':'text/html'})
-        res.write()
-        res.end()
-
-
-    }
-    //styles
-    else if(url === '/styles.css' ) {
-        res.writeHead(200, {'content-type':'text/css'})
-        res.write(homeStyles)
-        res.end()
-
-
-    }
-    else if(url === '/logo.svg' ) {
-        res.writeHead(200, {'content-type':'image/svg+xml'})
-        res.write(homeImage)
-        res.end()
-    }
-    else if(url === '/logo.svg' ) {
-        res.writeHead(200, {'content-type':'image/svg+xml'})
-        res.write(homeImage)
-        res.end()
-
-
-    }
-
-    else if(url === '/browser-app.js' ) {
-        res.writeHead(200, {'content-type':'text/javascript'})
-        res.write(homeLogic)
-        res.end()
-
-
-    }
-    //404
-    else{
-        res.writeHead(400, {'content-type':'text/html'})
-        res.write('<h2> no home page</h2>')
-        res.end()
-    }
+app.get('/', (req,res) => {
+    res.send('hello homepage')
+})
+app.get('/about', (req,res) => {
+    res.send('about homepage')
 
 })
 
-server.listen(5000)
-
-/*
-const http = require('http)
-const server = http.createServer((req,res) => {
-    console.log('user hit diff...)
-    res.end ('home...)
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>resource not found</h1>')
 })
-
-
-*/ 
+app.listen(5000, () => {
+    console.log('server is listening on port 5000')
+})
