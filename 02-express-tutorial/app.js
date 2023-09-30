41c72f4d1cbd185744e97ca6d1cc1ed782b238ba
2sammy <samuelmanoah61@gmail.com>
2023-09-30 13:34:55 +0300

    jquerry

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -16,10 +16,40 @@ app.get('/api/products/', (req, res) => {
 
 })
 
-app.get('/api/products/1', (req, res) => {
-    const singleProduct = products.find((product) => product.id ===1)
-    res.json(singleProduct)
-})
+app.get('/api/products/:productID', (req, res) => {
+    // console.log(req)
+    // console.log(req.params)
+    const { productID } = req.params
+  
+    const singleProduct = products.find(
+      (product) => product.id === Number(productID)
+    )
+    if (!singleProduct) {
+      return res.status(404).send('Product Does Not Exist')
+    }
+  
+    return res.json(singleProduct)
+  })
+  app.get('/api/v1/query', (req, res) => {
+    // console.log(req.query)
+    const { search, limit } = req.query
+    let sortedProducts = [...products]
+  
+    if (search) {
+      sortedProducts = sortedProducts.filter((product) => {
+        return product.name.startsWith(search)
+      })
+    }
+    if (limit) {
+      sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    if (sortedProducts.length < 1) {
+      // res.status(200).send('no products matched your search');
+      return res.status(200).json({ sucess: true, data: [] })
+    }
+    res.status(200).json(sortedProducts)
+  })
+  
 
 
 app.listen(5000, () => {
