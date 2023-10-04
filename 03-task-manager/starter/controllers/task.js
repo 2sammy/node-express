8455371b9d4fe27dcff3a1722581e85fc25ced93
2sammy <samuelmanoah61@gmail.com>
2023-10-04 17:19:38 +0300

    valiadation

--- 03-task-manager/starter/controllers/task.js
+++ 03-task-manager/starter/controllers/task.js
@@ -3,9 +3,16 @@ const Task = require('../models/task')
 const getAllTasks = (req, res) => {
     res.send('get all tasks')
 }
-const createTask =async (req, res) => {
+const createTask = async (req, res) => {
+    try{
     const task = await Task.create(req.body)
-    res.status(200).json(task)
+    res.status(201).json({task})
+}
+    catch(error) {
+        res.status(500).json({msg : error})
+
+    }
+
 }
 const getTask = (req, res) => {
     res.json({id:req.params.id})
--- 03-task-manager/starter/models/task.js
+++ 03-task-manager/starter/models/task.js
@@ -1,8 +1,21 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    name: String,
-    completed: Boolean
+    name: { 
+        type: String,
+        requre: [true, 'must provide a name'],
+        trim:true,
+        maxlength:[20, 'name can not be more than 20 characters']
+    
+    
+    },
+    
+    completed:{
+        type: Boolean,
+        default: false
+
+    
+    }
 
 })
 
