f85502145ac96ea68d7ab1c8619ecd50fe364924
2sammy <samuelmanoah61@gmail.com>
2023-10-04 15:57:01 +0300

    mongoose

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose')
 
 
-const connnectionString = 'mongodb+srv://samuelmanoah:Say6102@node-express.2rbquvg.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority'
 
 const connectDB = (url) => {
     return mongoose.
-    connect(connnectionString, {
+    connect(url, {
         useUnifiedTopology:true,
         useFindAndModify: false,
         useCreateIndex: true,
