4dc45a04ddb4a6eaf8d6e2ce0f6b8aec9467ad37
2sammy <samuelmanoah61@gmail.com>
2023-10-05 13:01:37 +0300

    errorhandler

--- 03-task-manager/starter/controllers/task.js
+++ 03-task-manager/starter/controllers/task.js
@@ -1,5 +1,6 @@
 const Task = require('../models/task')
 const asyncWrapper = require('../middleware/async')
+const {createCustomError} = require('../errors/custom-error')
 
 const getAllTasks = asyncWrapper(async(req, res) => {
     const tasks = await Task.find({})
@@ -17,7 +18,9 @@ const getTask =asyncWrapper( async(req, res) => {
     const {id: taskID} = req.params
     const task = await Task.findOne({_id:taskID})
     if(!task){
-        return res.status(404).json({msg: `No task with id : ${taskID}`})
+        return next(createCustomError('No task with id : ${taskID}`'))
+        
+
     }
 
     res.status(200).json({task})
@@ -35,8 +38,8 @@ const updateTask = asyncWrapper(async (req, res) => {
     })
 
     if(!task) {
-        return res.status(404).json({msg :`no id with such : ${taskID}`})
-}
+        return next(createCustomError(`no id with such : ${taskID}`))}
+
 })
 
 const deleteTask = asyncWrapper (async(req, res) => {
--- 03-task-manager/starter/middleware/errorHandler.js
+++ 03-task-manager/starter/middleware/errorHandler.js
@@ -1,5 +1,9 @@
+const {CustomAPIError} = require('../errors/custom-error')
 const errorHandlermiddleware = (err,req,res,next) => 
 {
+    if(err instanceof CustomAPIError) {
+        return res.status(err.statusCode).json({msg: err.message})
+    }
     return res.status(500).json({msg: "something went wrong, try again later"})
 }
 module.exports = errorHandlermiddleware
