afa49e3ba79c1ef248b8b758fdcb55ab960db2dc
2sammy <samuelmanoah61@gmail.com>
2023-10-08 14:20:44 +0300

    routes

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -5,6 +5,8 @@ const express = require('express')
 const app = express();
 
 const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
+
 
 
 const notFoundMiddleware = require('./middleware/not-found')
@@ -17,7 +19,7 @@ app.get('/', (req, res) => {
     res.send('<h1>Store API</h1> <a href="/api/v1/products"> products route </a>')
 })
 
-app.use('/api/v1/products',)
+app.use('/api/v1/products', productsRouter)
 
 //products route
 app.use(notFoundMiddleware)
